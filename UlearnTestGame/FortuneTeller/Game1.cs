4ed9aa35cc450e1a9f4244eaed85db2b366f6d8a
alina-koptelova <alina0901200432ya.ru>
2023-04-27 16:19:33 +0500

    sprites add

--- UlearnTestGame/FortuneTeller/Game1.cs
+++ UlearnTestGame/FortuneTeller/Game1.cs
@@ -8,6 +8,11 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    private Texture2D background;
+    private Texture2D door;
+    private Texture2D cardsDeck;
+    private Texture2D book;
+    private float scale;
 
     public Game1()
     {
@@ -18,6 +23,12 @@ public class Game1 : Game
 
     protected override void Initialize()
     {
+        _graphics.PreferredBackBufferWidth = 1200;
+        _graphics.PreferredBackBufferHeight = 675;
+        _graphics.ApplyChanges();
+
+        // Вычисление масштабного коэффициента
+        scale = _graphics.PreferredBackBufferWidth / 1920f;
         // TODO: Add your initialization logic here
 
         base.Initialize();
@@ -26,6 +37,10 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+        background = Content.Load<Texture2D>("room");
+        door = Content.Load<Texture2D>("door");
+        cardsDeck = Content.Load<Texture2D>("cards");
+        book = Content.Load<Texture2D>("book");
 
         // TODO: use this.Content to load your game content here
     }
@@ -44,6 +59,16 @@ public class Game1 : Game
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(background, Vector2.Zero, null, Color.White,
+            0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+        _spriteBatch.Draw(door, Vector2.Zero, null, Color.White,
+            0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+        _spriteBatch.Draw(book, Vector2.Zero, null, Color.White,
+            0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+        _spriteBatch.Draw(cardsDeck, Vector2.Zero, null, Color.White,
+            0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+        _spriteBatch.End();
 
         // TODO: Add your drawing code here
 
